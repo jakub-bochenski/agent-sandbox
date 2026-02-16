26b7a2e9907e292db562b44762c130f667b2d441
jbochenski <jbochenski@virtuslab.com>
2026-02-16 17:33:02 +0100

    fix: Don't shim yq

--- cli/lib/require.bash
+++ cli/lib/require.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-# Ensures a command is available, optionally shimming it via Docker if supported.
-# If the command is not found and a shim exists, creates a function that runs
-# the command in a Docker container. If no shim exists, returns an error.
+# Ensures a command is available
 # Args:
 #   $1 - The command name to require
 # Returns:
@@ -13,43 +11,8 @@ require() {
 
 	if ! command -v "$cmd" &>/dev/null
 	then
-
-		local shim_image
-		case "$cmd" in
-		yq)
-			shim_image="mikefarah/yq"
-			;;
-		*)
-			shim_image=""
-			;;
-		esac
-
-		if [[ -n "$shim_image" ]] && command -v docker &>/dev/null
-		then
-			>&2 echo "$0: $cmd not found, using Docker cmd"
-			docker pull --quiet "$shim_image"
-			eval "$cmd"'() {
-					docker run \
-						--rm \
-						--interactive \
-						--volume "$PWD:$PWD" \
-						--workdir "$PWD" \
-						--user "$(id -u):$(id -g)" \
-						--network=host \
-						'"$shim_image"' "$@"
-				}'
-			local output
-			if ! output=$("$cmd" --version 2>&1)
-			then
-				>&2 echo "$0: $cmd failed to run"
-				>&2 echo "$output"
-				unset "$cmd"
-				return "$exitcode_expectation_failed"
-			fi
-		else
 			>&2 echo "$0: $cmd required"
 			return "$exitcode_expectation_failed"
-		fi
 	fi
 }
 
